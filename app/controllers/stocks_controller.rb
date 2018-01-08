bb6db7b449f04787ce6cd7b79e5cc61264ae39e1
Viviana Bhandari <vbhandari@DB-MBP-Q1G3QD.local>
2018-01-07 22:00:51 -0800

    Add search stock ajax and errors

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,15 +1,14 @@
 class StocksController < ApplicationController
-  def search
-    if params[:stock]
-      @stock = Stock.find_by_ticker(params[:stock])
-      @stock ||= Stock.new_from_lookup(params[:stock])
-    end
 
-    if @stock
-      # render json: @stock
-      render partial: 'lookup'
+  def search
+    if params[:stock].blank?
+      flash.now[:danger] = "You have entered an empty search string"
     else
-      render status: :not_found, nothing: true
+      @stock = Stock.new_from_lookup(params[:stock])
+      flash.now[:danger] = "You have entered an incorrect symbol" unless @stock
+    end
+    respond_to do |format|
+      format.js { render partial: 'users/result' }
     end
   end
 end
